a03a807da977d86621a3f181d0ffa0c6aeca03d8
nathan hains <nathanhains14@yahoo.com>
2021-02-20 17:33:48 -0500

    Created method which will act as a failsafe for unprompted user input. Also returns input as an integer.

--- lib/inspirational_stress_reliever/cli.rb
+++ lib/inspirational_stress_reliever/cli.rb
@@ -21,4 +21,15 @@ class InspirationalStressReliever::CLI
         options_until(input)
     end
 
+    def options_until(input)
+        until input == '1' || input == '2'
+            puts "I know you are stressed but please read carefully..."
+            print "Enter 1 for 'yes', 2 for 'no: "
+            input = gets.strip
+            puts ""
+        end
+        input.to_i
+    end
+
+    
 end
