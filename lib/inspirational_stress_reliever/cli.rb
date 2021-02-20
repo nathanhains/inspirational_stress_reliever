cbfe5e161442a1e6709ccf29b012f49fc6daf88d
nathan hains <nathanhains14@yahoo.com>
2021-02-20 17:25:22 -0500

    I created my hello method which will greet the user with the title & the first question.

--- lib/inspirational_stress_reliever/cli.rb
+++ lib/inspirational_stress_reliever/cli.rb
@@ -1,8 +1,17 @@
-class InspirationalStressReliever::cli
+class InspirationalStressReliever::CLI
     def call
         hello
         start
         goodbye
+    end
+
+    def hello
+        puts ""
+        puts "-----------------------------------------"
+        puts "Welcome to Inspirational Stress Reliever!"
+        puts "-----------------------------------------"
+        puts ""
+        puts "> Are you feeling stressed today?"
     end
 
 end
