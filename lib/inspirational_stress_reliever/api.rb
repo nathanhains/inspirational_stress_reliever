748f6f4259933635d0e8e2a5f9a1a4bc2f0fccdb
nathan hains <nathanhains14@yahoo.com>
2021-02-20 17:53:18 -0500

    Added API instance methods to pull the quote from the url call, find the set of data we need, get the specific quote and then print the quote.

--- lib/inspirational_stress_reliever/api.rb
+++ lib/inspirational_stress_reliever/api.rb
@@ -1,3 +1,19 @@
 class InspirationalStressReliever::API
+    def initialize
+        @url = "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en"
+    end
+
+    def find_data
+        response_hash = HTTParty.get(@url)
+        self.get_quote(response_hash)
+    end
+
+    def get_quote(find_data)
+        find_data["quoteText"]
+    end
+
+    def print_quote
+        puts find_data
+    end
 
 end
